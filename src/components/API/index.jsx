fdc90528e6350e37809d01d394fa418a79c54d55
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-05 23:14:05 +0200

    created the endpoints to use

---
+++ src/components/API/index.jsx
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://v2.api.noroff.dev/holidaze";
+export const LOGIN_ENDPOINT_URL = "https://v2.api.noroff.dev/auth/login";
+export const REGISTER_ENDPOINT_URL = "https://v2.api.noroff.dev/auth/register";
+export const CreateApiKeyUrl = "https://v2.api.noroff.dev/auth/create-api-key";
