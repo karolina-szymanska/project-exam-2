c42ee5e42ee18b998f0449eb60125ffc32956572
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 01:28:12 +0200

    updated an icon

--- src/components/Layout/Logo/index.jsx
+++ src/components/Layout/Logo/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { RiHome2Line } from "react-icons/ri";
+import { GiPalmTree } from "react-icons/gi";
 
 const Logo = ({ colorClass }) => {
   return (
     <div className={colorClass || "text-violet-700"}>
       {" "}
       <Link to="/" className="flex items-center">
-        <RiHome2Line size={40} />
+        <GiPalmTree size={40} />
         <h1 className="mt-2 text-2xl">Holidaze</h1>
       </Link>
     </div>
