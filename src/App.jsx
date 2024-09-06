9ff8af29d5c3b71e29df1310fe2990b98e189b1e
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 02:07:12 +0200

    added import and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
 import Layout from "./components/Layout";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
         </Route>
       </Routes>
     </Router>
