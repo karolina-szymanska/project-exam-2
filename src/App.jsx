d03fd8260e462e5842ffbdd3ee3a654b8f52689e
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 16:03:38 +0200

    added import and route for listings page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import ListOfVenuesPage from "./Pages/ListOfVenuesPage";
 import AboutPage from "./Pages/AboutPage";
 import Layout from "./components/Layout";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="listings" element={<ListOfVenuesPage />} />
           <Route path="about" element={<AboutPage />} />
         </Route>
       </Routes>
