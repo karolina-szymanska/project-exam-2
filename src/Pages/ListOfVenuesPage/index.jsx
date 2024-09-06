4e92e584409768f6404c4e47ae74df7ed60093b0
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 16:05:40 +0200

    the function including listings and scroll to top element

---
+++ src/Pages/ListOfVenuesPage/index.jsx
@@ -0,0 +1,13 @@
+import ProductList from "../../components/ProductList";
+import ScrollToTop from "../../components/ScrollToTop";
+
+const ListOfVenuesPage = () => {
+  return (
+    <div className="mx-auto max-w-screen-lg">
+      <ProductList />
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default ListOfVenuesPage;
