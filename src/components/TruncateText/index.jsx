4c14313f8050ad5cd25fb5f40d0338fc7a3207c9
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 23:16:24 +0200

    added truncate text component

---
+++ src/components/TruncateText/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+/**
+ * Component to truncate text and toggle between truncated and full text.
+ *
+ * @param {Object} props - Component props.
+ * @param {string} props.text - The text to be truncated.
+ * @param {number} props.maxLength - The maximum length of the truncated text.
+ *
+ * @component
+ * @example
+ * const exampleText = "This is an example of a long text that needs to be truncated.";
+ * const maxLength = 20;
+ * return (
+ *   <TruncateText text={exampleText} maxLength={maxLength} />
+ * );
+ */
+const TruncateText = ({ text, maxLength }) => {
+  const [isTruncated, setIsTruncated] = useState(true);
+
+  const handleToggle = () => {
+    setIsTruncated(!isTruncated);
+  };
+
+  const truncatedText =
+    isTruncated && text.length > maxLength
+      ? text.substring(0, maxLength) + "..."
+      : text;
+
+  return (
+    <div>
+      <span>{truncatedText}</span>
+      {text.length > maxLength && (
+        <button onClick={handleToggle} className="text-sm text-violet-700">
+          ({isTruncated ? "View More" : "View Less"})
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default TruncateText;
