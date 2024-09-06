a11a970d1894f078dec86346ad2d8ed2d5892143
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-06 16:04:50 +0200

    the function for scrolling to top element

---
+++ src/components/ScrollToTop/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="pointer-events-none fixed bottom-7 left-1/2 -translate-x-1/2 transform">
+      {isVisible && (
+        <button
+          type="button"
+          onClick={scrollToTop}
+          className="pointer-events-auto flex items-center rounded-full bg-gradient-to-t from-violet-500 to-violet-700 p-2 text-white shadow-lg hover:to-violet-900 hover:font-semibold focus:outline-none focus:ring-2 focus:ring-violet-800 focus:ring-opacity-75"
+        >
+          <FaArrowUp className="mr-2 h-6 w-6" />
+          <span>Back to Top</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
