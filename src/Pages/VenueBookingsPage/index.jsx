5e751e78c97f689daf9b0887c70b74e8d30286a5
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-10 02:27:25 +0200

    general function for displaying venue booking page

---
+++ src/Pages/VenueBookingsPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VenueBookings from "../../components/Profile/VenueManager/VenuesBookings";
+
+const VenueBookingPage = () => {
+  return (
+    <div className="mx-auto max-w-screen-md">
+      <VenueBookings />
+    </div>
+  );
+};
+
+export default VenueBookingPage;
