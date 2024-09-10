b4f4ae81b46303d6112d0a5a85f43c537ce510f9
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-09-10 02:10:43 +0200

    added imports and routes for 3 pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import HomePage from "./Pages/HomePage";
 import ListOfVenuesPage from "./Pages/ListOfVenuesPage";
 import AboutPage from "./Pages/AboutPage";
 import Layout from "./components/Layout";
+import ProfilePage from "./Pages/ProfilePage";
 import LoginPage from "./Pages/LoginPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 import VenueDetailsPage from "./Pages/VenueDetailsPage";
 import RegisterPage from "./Pages/RegisterPage";
+import VenueImages from "./Pages/VenueDetailsPage/ImageCarousel";
+import VenueBookingPage from "./Pages/VenueBookingsPage";
 
 function App() {
   return (
@@ -16,7 +19,10 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="listings" element={<ListOfVenuesPage />} />
           <Route path="venue/:id" element={<VenueDetailsPage />} />
+          <Route path="venue/images/:id" element={<VenueImages />} />
+          <Route path="venue/bookings/:id" element={<VenueBookingPage />} />
           <Route path="about" element={<AboutPage />} />
+          <Route path="profile" element={<ProfilePage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="*" element={<NotFoundPage />} />
